5165ffb918074e3c5177ee212fe35d8cf05e722b
AliShamil <elisamilzade@gmail.com>
2023-03-26 23:46:49 +0400

    Add project files.

---
+++ ModelsLib/KeyValue.cs
@@ -0,0 +1,7 @@
+﻿namespace ModelsLib;
+
+public class KeyValue
+{
+    public char Key { get; set; }
+    public int Value { get; set; }
+}
---
+++ Server/Contexts/CacheDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ModelsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Contexts;
+
+public class CacheDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        var connectionString = configuration.GetConnectionString("ConStr");
+
+        optionsBuilder.UseSqlServer(connectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<KeyValue>().HasKey(e => e.Key);
+        base.OnModelCreating(modelBuilder);
+    }
+
+   public DbSet<KeyValue>? KeyValues { get; set; }
+}
